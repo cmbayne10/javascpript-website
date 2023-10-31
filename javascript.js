afdb3009bc21a330c267cf64285912227b0da170
cmbayne10 <cmbayne10@gmail.com>
2023-10-31 12:55:35 -0400

    commit

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 function onClick(inputName) {
      document.getElementById("output").innerHTML = "thanks, I'm now overlord of the world"
-     alert('Hello Give Me your bank account numbers ' + inputName +'!!!!!!!')
+     alert('Hello Give Me your bank account number ' + inputName +'!!!!!!!')
 }
