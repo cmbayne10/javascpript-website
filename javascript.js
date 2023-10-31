457b706ab5200bb8d6c64c59d80bc440d63d24e9
cmbayne10 <cmbayne10@gmail.com>
2023-10-31 12:13:19 -0400

    java script

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function onClick(inputName) {
+     document.getElementById("output").innerHTML = "thanks, I'm now overlord of the world"
+     alert('Hello Give Me your bank account numbers ' + inputName +'!!!!!!!')
+}
