fb8ae1df6a85966e218b5e6b56638e4c6fe142f2
cmbayne10 <cmbayne10@gmail.com>
2023-10-31 13:16:09 -0400

    commiy

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 function onClick(inputName) {
-     document.getElementById("output").innerHTML = "thanks, I'm now overlord of the world"
+     document.getElementById("output").innerHTML = "thanks, I'm now overlord of the world HA HA HA HA"
      alert('Hello Give Me your bank account number ' + inputName +'!!!!!!!')
 }
