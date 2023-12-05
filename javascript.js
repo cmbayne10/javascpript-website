d62c3c9c8832bc90f69469ae97232bb677a915c4
cmbayne10 <cmbayne10@gmail.com>
2023-12-05 13:27:10 -0500

    yo

--- javascript.js
+++ javascript.js
@@ -1,22 +1,13 @@
 function onPress(inputName) {
-    document.getElementById("displayedText").innerHTML = "thanks, I'm now overlord of the world HA HA HA";
-    const user = {name: ' bob'};
-    console.log ('Hello' + user.name);
-
-    const names = ['Sally', 'jerome']
-    const users = [{name: 'Sally'}, {name: 'jerome'}];
-    console.log('First name in list -----',names[0]);
-
-    console.log ('Second name in list of objects -----',users[1].name);
     const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
-            const pokemonNames = data.results.map(showNames) 
-            console.log(pokemonNames);
+            console.log(data);
+            const pokemonNames = data.results.map(pokemon => pokemon.name) 
             document.getElementById("displayedText").innerHTML = pokemonNames.join('\n');
         }
     }); 
-    alert('Hello, ' + inputName + '!');
+   
 
 }
 
