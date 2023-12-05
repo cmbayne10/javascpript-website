d741e9f47d2ca4d8285390fd64edf96782668dff
cmbayne10 <cmbayne10@gmail.com>
2023-12-05 12:52:39 -0500

    finaly

--- javascript.js
+++ javascript.js
@@ -1,20 +1,25 @@
-function onClick(inputName) {
-    document.getElementById("insertedName").innerHTML = "thanks, I'm now overlord of the world HA HA HA";
-    const names = ['Sally', 'Jerome']
-    const users = [{name: 'Sally'}, {name: 'Jerome'}]
-    console.log ('First name in list -----',names[0]);
+function onPress(inputName) {
+    document.getElementById("displayedText").innerHTML = "thanks, I'm now overlord of the world HA HA HA";
+    const user = {name: ' bob'};
+    console.log ('Hello' + user.name);
+
+    const names = ['Sally', 'jerome']
+    const users = [{name: 'Sally'}, {name: 'jerome'}];
+    console.log('First name in list -----',names[0]);
 
     console.log ('Second name in list of objects -----',users[1].name);
-    const url = 'https://pokeai.co/api/v2/pokemon';
+    const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
-        if (status === 200) {
-console.log(data);            
+        if (status === 'success') {
+            const pokemonNames = data.results.map(showNames) 
+            console.log(pokemonNames);
+            document.getElementById("displayedText").innerHTML = pokemonNames.join('\n');
         }
     }); 
-    alert('Hello, ' + insertedName + '!');
+    alert('Hello, ' + inputName + '!');
 
 }
 
 function showNames(pokemon) {
-    return pokemon.name
+    return pokemon.name 
 }
